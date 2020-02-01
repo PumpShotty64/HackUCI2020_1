077c817f544ff1c9dbb7bd0b06fee51904a480ff
Alexis Chavoya <chavoyaa@uci.edu>
2020-02-01 08:48:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 # main.py
 
-import pygame
+import pygame as pg
+import traceback
+import random
 
-WINDOWHEIGHT = 720
+# Pygame constaants
 WINDOWWIDTH  = 1280
-WINDOW       = (WINDOWHEIGHT, WINDOWWIDTH)
+WINDOWHEIGHT = 720
+RESOLUTION   = (WINDOWWIDTH, WINDOWHEIGHT)
+WINDOWNAME   = "HackUCI2020"
+WHITE        = (255, 255, 255)
+BLACK        = (0, 0, 0)
 
-pygame.init()
+# Pygame setup
+pg.init()
+# pg.display.set_icon(pg.image.load("someImage.png"))
+pg.display.set_caption(WINDOWNAME)
+GAMEDISPLAY  = pg.display.set_mode(RESOLUTION)
+CLOCK        = pg.time.Clock()
 
+# Game constants
+# BACKGROUND  = pg.image.load("")
+GENERALFONT = pg.font.SysFont(None, 32)
+DEBUGFONT   = pg.font.SysFont(None, 200)
+DEBUGGERMSG = DEBUGFONT.render("HELLO", True, WHITE)
+
+
+GAMEDISPLAY.blit(DEBUGGERMSG, (100, 100)) # arbitrary position
+pg.display.update()
+
+input("PAUSE")
+
+pg.quit()
+quit()
