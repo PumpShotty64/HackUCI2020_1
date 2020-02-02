26dcabe19629d8b7fea2c0a81d1497d7560182c6
PumpShotty64 <jordanguyent@gmail.com>
2020-02-01 16:29:04 -0800

    Update Player_Movements.py

--- Player_Movements.py
+++ Player_Movements.py
@@ -8,12 +8,19 @@ def movement(p1, p2, event):
     p1.set_sprite(0)
     p2.set_sprite(0)
 
-    if (keys[pg.K_a]): # LEFT
-        p1.inc_x(-p1.SPEED)
-        p1.set_sprite(2)
-    if (keys[pg.K_d]): # RIGHT
-        p1.inc_x(p1.SPEED)
-        p1.set_sprite(2)
+    if (keys[pg.K_s]): # DOWN
+        p1.is_crouching = True;
+        p1.set_sprite(1)
+    if (not p1.is_crouching or (p1.is_crouching and p1.is_jumping)):
+        if (keys[pg.K_a]): # LEFT
+            p1.inc_x(-p1.SPEED)
+            if (not p1.is_crouching): p1.set_sprite(2)
+        if (keys[pg.K_d]): # RIGHT
+            p1.inc_x(p1.SPEED)
+            if (not p1.is_crouching): p1.set_sprite(2)
+        if (keys[pg.K_w]): # UP
+            p1.set_yv(-p1.JUMP)
+
 
 
     if (keys[pg.K_LEFT]): # LEFT
